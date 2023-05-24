1dfd2f034ecf8e2c4682cde5fcffd43c78ddbb62
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-05-24 16:36:46 +0600

    basic setup done combining laravel, vue and inertia

---
+++ resources/js/Page/create.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Laravel 10 vite with vue 3
+    </h1>
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+
+import app from './Page/create.vue'
+
+createApp(app).mount("#app")
